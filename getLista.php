5eba7f5ac15c737c62232357c81d7e7501bcf2cf
leandro <leandro@leandro-devel.eacnet.intra>
2014-08-18 17:56:47 -0300

    Melhoria no ws de lista, trazendo agora lista por clientes

--- getLista.php
+++ getLista.php
@@ -13,7 +13,8 @@ $log = new Log();
 
 /* obtendo algumas configuracoes do sistema */
 $conf = getConfig();
-$ws = sprintf("%s/listaws.php", $conf["SISTEMA"]["saciWS"]);
+$wsLista = sprintf("%s/listaws.php", $conf["SISTEMA"]["saciWS"]);
+$wsCliente = sprintf("%s/clientews.php", $conf["SISTEMA"]["saciWS"]);
 
 /* loja padrao */
 $loja = $conf["MISC"]["loja"];
@@ -33,78 +34,185 @@ $lista = $dados["lista"];
 $wsstatus = 0;
 $wsresult = array();
 
-// url de ws
-$client = new nusoap_client($ws);
-$client->useHTTPPersistentConnection();
-
 // serial do cliente
 $serail_number_cliente = readSerialNumber();
 
-$dados = "<lista>";
+if (empty($lista['nome_cliente'])) {
+// url de ws
+    $client = new nusoap_client($wsLista);
+    $client->useHTTPPersistentConnection();
 
-foreach ($lista as $key => $value) {
+    $dados = "<lista>";
 
-    if (!empty($value) || is_numeric($value)) {
-        $dados .= sprintf("<%s>%s</%s>\n", $key, $value, $key);
-    } else {
-        $dados .= sprintf("<%s/>\n", $key);
+    foreach ($lista as $key => $value) {
+
+        if (!empty($value) || is_numeric($value)) {
+            $dados .= sprintf("<%s>%s</%s>\n", $key, $value, $key);
+        } else {
+            $dados .= sprintf("<%s/>\n", $key);
+        }
     }
-}
 
-$dados .= "</lista>";
+    $dados .= "</lista>";
 
 // grava log
-$log->addLog(ACAO_REQUISICAO, "getLista", $dados, SEPARADOR_INICIO);
+    $log->addLog(ACAO_REQUISICAO, "getLista", $dados, SEPARADOR_INICIO);
 
 // monta os parametros a serem enviados
-$params = array(
-    "crypt" => $serail_number_cliente,
-    "dados" => $dados
-);
+    $params = array(
+        "crypt" => $serail_number_cliente,
+        "dados" => $dados
+    );
 
 // realiza a chamada de um metodo do ws passando os paramentros
-$result = $client->call("listar", $params);
+    $result = $client->call("listar", $params);
 
 // remove acentos dos dados
-$result = removerAcentos($result);
+    $result = removerAcentos($result);
 
 // grava log
-$log->addLog(ACAO_RETORNO, "dadosLista", $result);
+    $log->addLog(ACAO_RETORNO, "dadosLista", $result);
 
 // converte o xml em um array
-$res = XML2Array::createArray($result);
+    $res = XML2Array::createArray($result);
 
-if ($res["resultado"]["sucesso"] && isset($res["resultado"]["dados"]["lista"])) {
+    if ($res["resultado"]["sucesso"] && isset($res["resultado"]["dados"]["lista"])) {
 
-    $listas = array();
+        $listas = array();
 
-    if (key_exists("0", $res["resultado"]["dados"]["lista"]))
-        $listas = $res["resultado"]["dados"]["lista"];
-    else
-        $listas[] = $res["resultado"]["dados"]["lista"];
+        if (key_exists("0", $res["resultado"]["dados"]["lista"]))
+            $listas = $res["resultado"]["dados"]["lista"];
+        else
+            $listas[] = $res["resultado"]["dados"]["lista"];
 
-    $wsstatus = 1;
-    $wsresult = array();
+        $wsstatus = 1;
+        $wsresult = array();
 
-    foreach ($listas as $lista) {
-        /* dados do produto */
-        $wsresult[] = array(
-            "cliente_codigo" => $lista["codigo_cliente"],
-            "cliente_name" => $lista["name_cliente"],
-            "tipo_codigo" => $lista["tipo"],
-            "tipo_name" => $lista["tipo_name"],
-            "data_evento" => $lista["data_evento"]
-        );
+        foreach ($listas as $lista) {
+            /* dados do produto */
+            $wsresult[] = array(
+                "cliente_codigo" => $lista["codigo_cliente"],
+                "cliente_nome" => $lista["name_cliente"],
+                "tipo_codigo" => $lista["tipo"],
+                "tipo_nome" => $lista["tipo_name"],
+                "data_evento" => $lista["data_evento"]
+            );
+        }
+    } else {
+        /* monta o xml de retorno */
+        $wsstatus = 0;
+        $wsresult["wserror"] = "Nenhuma lista encontrada!";
+
+        // grava log
+        $log->addLog(ACAO_RETORNO, "", $wsresult, SEPARADOR_FIM);
+
+        returnWS($wscallback, $wsstatus, $wsresult);
     }
 } else {
-    /* monta o xml de retorno */
-    $wsstatus = 0;
-    $wsresult["wserror"] = "Nenhuma lista encontrada!";
 
-    // grava log
-    $log->addLog(ACAO_RETORNO, "", $wsresult, SEPARADOR_FIM);
+    $lista['nome_cliente'] = sprintf("%%%s%%", $lista['nome_cliente']);
 
-    returnWS($wscallback, $wsstatus, $wsresult);
+    //url de ws
+    $client = new nusoap_client($wsCliente);
+    $client->useHTTPPersistentConnection();
+
+    $dados = sprintf("<dados><nome_cliente>%s</nome_cliente></dados>", $lista['nome_cliente']);
+
+// grava log
+    $log->addLog(ACAO_REQUISICAO, "getCliente", $dados, SEPARADOR_INICIO);
+
+// monta os parametros a serem enviados
+    $params = array(
+        "crypt" => $serail_number_cliente,
+        "dados" => $dados
+    );
+
+// realiza a chamada de um metodo do ws passando os paramentros
+    $result = $client->call("listar", $params);
+
+// remove acentos dos dados
+    $result = removerAcentos($result);
+
+// grava log
+    $log->addLog(ACAO_RETORNO, "dadosCliente", $result);
+
+// converte o xml em um array
+    $res = XML2Array::createArray($result);
+
+    if ($res["resultado"]["sucesso"] && isset($res["resultado"]["dados"]["cliente"])) {
+
+        $clientes = array();
+
+        if (key_exists("0", $res["resultado"]["dados"]["cliente"]))
+            $clientes = $res["resultado"]["dados"]["cliente"];
+        else
+            $clientes[] = $res["resultado"]["dados"]["cliente"];
+
+        $wsstatus = 1;
+        $wsresult = array();
+
+        foreach ($clientes as $cliente) {
+
+            $client = new nusoap_client($wsLista);
+            $client->useHTTPPersistentConnection();
+
+            $dados = sprintf("<lista><codigo_cliente>%s</codigo_cliente>"
+                    . "<tipo>%s</tipo>"
+                    . "<data_evento>%s</data_evento></lista>", 
+                    $cliente['codigo_cliente'], $lista['tipo'], $lista['data_evento']);
+
+// grava log
+            $log->addLog(ACAO_REQUISICAO, "getLista", $dados, SEPARADOR_INICIO);
+
+// monta os parametros a serem enviados
+            $params = array(
+                "crypt" => $serail_number_cliente,
+                "dados" => $dados
+            );
+
+// realiza a chamada de um metodo do ws passando os paramentros
+            $result = $client->call("listar", $params);
+
+// remove acentos dos dados
+            $result = removerAcentos($result);
+
+// grava log
+            $log->addLog(ACAO_RETORNO, "dadosLista", $result);
+
+// converte o xml em um array
+            $res = XML2Array::createArray($result);
+
+            if ($res["resultado"]["sucesso"] && isset($res["resultado"]["dados"]["lista"])) {
+
+                $listas = array();
+
+                if (key_exists("0", $res["resultado"]["dados"]["lista"]))
+                    $listas = $res["resultado"]["dados"]["lista"];
+                else
+                    $listas[] = $res["resultado"]["dados"]["lista"];
+
+                foreach ($listas as $list) {                   
+                    /* dados do produto */
+                    $wsresult[] = array(
+                        "cliente_codigo" => $list["codigo_cliente"],
+                        "cliente_nome" => $list["name_cliente"],
+                        "tipo_codigo" => $list["tipo"],
+                        "tipo_nome" => $list["tipo_name"],
+                        "data_evento" => $list["data_evento"]
+                    );
+                }
+            }
+        }
+    } else {
+        /* monta o xml de retorno */
+        $wsstatus = 0;
+        $wsresult["wserror"] = "Nenhuma lista encontrada!";
+
+        // grava log
+        $log->addLog(ACAO_RETORNO, "", $wsresult, SEPARADOR_FIM);
+
+        returnWS($wscallback, $wsstatus, $wsresult);
+    }
 }
 
 // grava log
