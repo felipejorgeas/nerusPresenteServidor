14dedf987be96f652f156744480d180504a60c24
Felipe Jorge <felipejorgeas@gmail.com>
2014-10-16 17:45:30 -0300

    Delete relFuncUser.php

--- config/relFuncUser.php
+++
@@ -1,8 +0,0 @@
-;<?php die(); /* DO NOT REMOVE THIS LINE! SERIOUS SECURITY RISK IF REMOVED! */ ?>
-; Relacionamento entre funcionario e usuario do SACI
-
-[RELACAO_FUNC_USER]
-1 = 1
-2 = 1
-6 = 1
-2152 = 1
